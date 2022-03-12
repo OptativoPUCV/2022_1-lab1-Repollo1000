34653d9034428755df0175ea67ee1687a841b174
Your Name <you@example.com>
2022-03-12 16:19:56 -0300

    new try

--- exercises.c
+++ exercises.c
@@ -109,7 +109,7 @@ la cual asigna el valor a la posición i del vector v.
 void asignarValor(Vector *v, int i, int valor)
 {
    int cont = 0;
-   while (i != cont)
+   while (v->capacidad != cont)
    {
 
       v->datos[i]=valor;
